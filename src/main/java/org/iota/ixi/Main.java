45cd585dd4b1dea36db38e4b4315515c8b064468
Lukas Tassanyi <lukas.tassanyi@iota.org>
2019-03-07 12:07:38 +0100

    update to ict 0.6-SNAPSHOT

--- src/main/java/org/iota/ixi/Main.java
+++ src/main/java/org/iota/ixi/Main.java
@@ -1,9 +1,8 @@
 package org.iota.ixi;
 
 import org.iota.ict.Ict;
-import org.iota.ict.ixi.IctProxy;
 import org.iota.ict.utils.Constants;
-import org.iota.ict.utils.Properties;
+import org.iota.ict.utils.properties.EditableProperties;
 
 /**
  * This class is just for testing your IXI, so you don't have to run Ict manually.
@@ -11,11 +10,12 @@ import org.iota.ict.utils.Properties;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Running IXI module for Ict version " + Constants.ICT_VERSION);
-        Properties properties = new Properties();
-        properties.guiEnabled = false;
-        Ict ict = new Ict(properties);
-        Module module = new Module(new IctProxy(ict));
+        EditableProperties properties = new EditableProperties();
+        properties.guiEnabled(false);
+        Ict ict = new Ict(properties.toFinal());
+        Module module = new Module(ict);
         new Thread(module).run();
     }
 }
--- src/main/java/org/iota/ixi/Module.java
+++ src/main/java/org/iota/ixi/Module.java
@@ -2,8 +2,10 @@ package org.iota.ixi;
 
 import org.iota.ict.ixi.Ixi;
 import org.iota.ict.ixi.IxiModule;
-import org.iota.ict.network.event.GossipEvent;
-import org.iota.ict.network.event.GossipListener;
+import org.iota.ict.model.transaction.Transaction;
+import org.iota.ict.model.transaction.TransactionBuilder;
+import org.iota.ict.network.gossip.GossipEvent;
+import org.iota.ict.network.gossip.GossipListener;
 
 /**
  * This is an example IXI module. Use it as template to implement your own module. Run Main.main() to test it.
@@ -20,18 +22,21 @@ public class Module extends IxiModule {
 
     public void run() {
         // submit a new transaction
-        ixi.submit("Hello world!");
+        TransactionBuilder builder = new TransactionBuilder();
+        builder.asciiMessage("Hello World!");
+        Transaction toSubmit = builder.build();
+        ixi.submit(toSubmit);
     }
 }
 
 /**
  * A custom gossip listener which prints every message submitted or received.
  * */
-class CutomGossipListener extends GossipListener {
+class CutomGossipListener implements GossipListener {
     @Override
     public void onGossipEvent(GossipEvent event) {
         String message = (event.isOwnTransaction() ? "SUBMITTED >>> " : "RECEIVED  <<< ")
-            + event.getTransaction().decodedSignatureFragments;
+            + event.getTransaction().decodedSignatureFragments();
         System.out.println(message);
     }
 }
